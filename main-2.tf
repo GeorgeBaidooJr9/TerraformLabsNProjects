fdf1d381e8e6007062f88720286edb6cc2e6eac7
George Baidoo Jr <96100899+GeorgeBaidooJr9@users.noreply.github.com>
2022-11-26 14:21:46 -0800

    updated: terraform 2-tier architecture

---
+++ main-2.tf
@@ -0,0 +1,128 @@
+#Creating a VPC with custom Public/Private subnets as well as an RDS MySQL Database
+
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
+  region = "us-east-1" 
+  }
+
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "terraform-tough-vpc"
+  cidr = "10.0.0.0/16"
+
+ #Establishing different AZS and associating them with public/private subnets 
+  azs             = ["us-east-1a", "us-east-1b"] 
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  
+  tags = {
+    Terraform ="true"
+    Environment = "free tier"
+  }
+
+  public_subnet_tags = {
+    Name = "amazing-public-subnets"
+  }
+  private_subnet_tags = {
+    Name = "private-amazing-subnets"
+  }
+
+  vpc_tags = {
+    Name = "terraform-tough-vpc"
+  }
+}
+
+  
+  #Creates an AWS MySQL database with set configuration
+resource "aws_db_instance" "default"{
+    allocated_storage    = 20
+    db_name              = "mysql_4_terraform"
+    engine               = "mysql"
+    engine_version       = "8.0"
+    instance_class       = "db.t3.micro"
+    username             = "orbitworld11"
+    password             = "foozhead22"
+    parameter_group_name = "default.mysql8.0"
+    skip_final_snapshot  = true
+    }
+
+#Creating EC2 instances on a public subnet 
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  for_each = toset(["one", "two"])
+
+  name = "terra-instance-${each.key}"
+
+  ami                    = "ami-0b0dcb5067f052a63"
+  instance_type          = "t2.micro"
+  key_name               = "lukey"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-06811e76fd8d88ef0"]
+  subnet_id              = "subnet-0a21dbfc65340945e"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+#Creating a load balancer for public subnets
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "~> 2.0"
+
+  name = "elb-4-terraform"
+
+  subnets         = ["subnet-0a21dbfc65340945e", "subnet-011c2265d2e318073"]
+  security_groups = ["sg-06811e76fd8d88ef0"]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = 80
+      instance_protocol = "HTTP"
+      lb_port           = 80
+      lb_protocol       = "HTTP"
+    },
+    {
+      instance_port     = 8080
+      instance_protocol = "http"
+      lb_port           = 8080
+      lb_protocol       = "http"
+    },
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+  
+  // ELB attachments to our instances
+  number_of_instances = 2
+  instances           = ["i-02f60ea653914a44b", "i-030f73330bd7d753a"]
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
+ 
