3ebc2522e43e99adbff5ba17453d74439a04470c
George Baidoo Jr <gbaidoo94@gmail.com>
2022-11-30 01:23:43 +0000

    terraform docker stuff

--- main.tf
+++ main.tf
@@ -11,3 +11,7 @@ provider "docker" {}
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.latest
+}
